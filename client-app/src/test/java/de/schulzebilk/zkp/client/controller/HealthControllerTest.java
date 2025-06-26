7a771047e0f3dc4dc27dac714c404c7f458d6d2e
nschubi <dev@schulzebilk.de>
2025-06-26 20:26:59 +0200

    [zero-trust-zkp-poc] add simple HealthController with health check

---
+++ client-app/src/test/java/de/schulzebilk/zkp/client/controller/HealthControllerTest.java
@@ -0,0 +1,29 @@
+package de.schulzebilk.zkp.client.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthController.class)
+class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthCheck() {
+        try {
+            mockMvc.perform(get("/health"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Client Application is running!"));
+        } catch (Exception e) {
+            fail("Health check failed: " + e.getMessage());
+        }
+    }
+}
---
+++ ressource-service/src/main/java/de/schulzebilk/zkp/ressource/controller/HealthController.java
@@ -0,0 +1,15 @@
+package de.schulzebilk.zkp.ressource.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "Resource Service is running!";
+    }
+}

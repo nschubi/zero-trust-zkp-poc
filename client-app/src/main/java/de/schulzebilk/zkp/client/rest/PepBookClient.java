c7de002ea84c823876b9923cf6dce0b43942a8aa
nschubi <dev@schulzebilk.de>
2025-07-15 17:08:17 +0200

    [client-app] print book after successful authentication

--- client-app/src/main/java/de/schulzebilk/zkp/client/rest/PepBookClient.java
+++ client-app/src/main/java/de/schulzebilk/zkp/client/rest/PepBookClient.java
@@ -4,14 +4,12 @@ import de.schulzebilk.zkp.client.auth.FiatShamirProver;
 import de.schulzebilk.zkp.core.auth.SessionState;
 import de.schulzebilk.zkp.core.dto.AuthenticationDTO;
 import de.schulzebilk.zkp.core.model.Book;
-import org.springframework.beans.factory.annotation.Autowired;
+import de.schulzebilk.zkp.core.util.AuthUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PepBookClient extends PepClient {
-
-    @Autowired
-    PepAuthClient pepAuthClient;
 
     public Book getBookById(Long id, FiatShamirProver prover) {
         var responseEntity = restClient.get()
@@ -20,24 +18,30 @@ public class PepBookClient extends PepClient {
                 .header("auth-payload", prover.getProverKey().toString())
                 .retrieve()
                 .onStatus(status -> status.value() == 401, (request, response) -> {
-                    var headers = response.getHeaders();
-                    AuthenticationDTO authenticate = new AuthenticationDTO(
-                            headers.getFirst("auth-user"),
-                            headers.getFirst("auth-session"),
-                            headers.getFirst("auth-payload"),
-                            SessionState.valueOf(headers.getFirst("auth-state")));
-                    while (authenticate.sessionState() != SessionState.VERIFIED && authenticate.sessionState() != SessionState.FAILED) {
+                    AuthenticationDTO authenticate = AuthUtils.createAuthenticationDtoFromHeaders(response.getHeaders());
+                    ResponseEntity<?> authResponse = null;
+                    while (authenticate.sessionState() != SessionState.VERIFIED
+                            && authenticate.sessionState() != SessionState.FAILED) {
                         System.out.println("Verifier: " + authenticate);
                         authenticate = prover.handleAuthentication(authenticate);
                         System.out.println("Prover: " + authenticate);
-                        authenticate = pepAuthClient.authenticate(authenticate);
+                        authResponse = sendAuthentication(authenticate);
+                        System.out.println("Headers from Auth Response: " + authResponse.getHeaders());
+                        authenticate = AuthUtils.createAuthenticationDtoFromHeaders(authResponse.getHeaders());
                     }
                     System.out.println("Final Authentication State: " + authenticate.sessionState());
-
+                    Book book = (Book) authResponse.getBody();
+                    System.out.println(book);
                 })
                 .toEntity(Book.class);
         System.out.println("Response Headers: " + responseEntity.getHeaders());
         return responseEntity.getBody();
     }
 
+    public ResponseEntity<?> sendAuthentication(AuthenticationDTO authenticationDTO) {
+       return restClient.post().uri("/api/authenticate")
+                .body(authenticationDTO)
+                .retrieve()
+                .toEntity(Book.class);
+    }
 }

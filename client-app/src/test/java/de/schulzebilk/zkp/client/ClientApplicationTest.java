f54986be3dde0eab4a82c5594ab296ad6f58f3ef
nschubi <dev@schulzebilk.de>
2025-07-22 18:58:32 +0200

    [client-app] mock FiatShamirPepClient in Test

--- client-app/src/test/java/de/schulzebilk/zkp/client/ClientApplicationTest.java
+++ client-app/src/test/java/de/schulzebilk/zkp/client/ClientApplicationTest.java
@@ -1,15 +1,24 @@
 package de.schulzebilk.zkp.client;
 
+import de.schulzebilk.zkp.client.rest.FiatShamirPepClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ClientApplicationTest {
 
+    @MockitoBean
+    private FiatShamirPepClient fiatShamirPepClient;
+
     @Test
     void testStart() {
+        when(fiatShamirPepClient.getPublicModulus()).thenReturn(BigInteger.valueOf(50000));
         assertTrue(true);
     }
 
